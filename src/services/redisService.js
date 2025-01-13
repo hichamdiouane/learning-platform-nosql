3eff5610de7c86ee77e592423785ff3d3f874422
HICHAM DIOUANE <hichamdiouane9@gmail.com>
2025-01-13 14:09:30 +0100

    Implementation de services de redis

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -6,8 +6,33 @@
 // Fonctions utilitaires pour Redis
 async function cacheData(key, data, ttl) {
     // TODO: Implémenter une fonction générique de cache
-  }
+    const redisClient = await redisdb.connectRedis();
+    try {
+      await redisClient.set(key, JSON.stringify(data), { EX: ttl });
   
-  module.exports = {
-    // TODO: Exporter les fonctions utilitaires
-  };
+    } catch (error) {
+      console.error("Error caching data:", error);
+    }
+}
+
+async function getCachedData(key) {
+  // get cash data implemenatation
+  const redisClient = await redisdb.connectRedis();
+  try {
+    const data = await redisClient.get(key);
+    if (data) {
+      return JSON.parse(data);
+    }
+    return null;
+  } catch (error) {
+    console.error("Error getting cached data:", error);
+    return null;
+  }
+
+}
+  
+module.exports = {
+  // TODO: Exporter les fonctions utilitaires
+  cacheData,
+  getCachedData
+};
