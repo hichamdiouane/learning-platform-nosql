325f4af635cee3c8a5a0470dac3ef75153b6d4c3
HICHAM DIOUANE <hichamdiouane9@gmail.com>
2025-01-13 11:58:49 +0100

    Implementation des connections aux mongodb and redis

--- src/config/db.js
+++ src/config/db.js
@@ -12,14 +12,58 @@ let mongoClient, redisClient, db;
 async function connectMongo() {
   // TODO: Implémenter la connexion MongoDB
   // Gérer les erreurs et les retries
+  mongoClient = new MongoClient(config.mongodb.uri);
+  try {
+    await mongoClient.connect();
+    db = mongoClient.db(config.mongodb.dbName);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("not connected from bd.js");
+  }
+  // Gérer les erreurs et les retries 
 }
 
 async function connectRedis() {
   // TODO: Implémenter la connexion Redis
   // Gérer les erreurs et les retries
+  const redisClient = redis.createClient();
+  try {
+    await redisClient.connect({
+      host: config.redis, port: config.port
+    });
+    console.log("data base connected");
+  } catch (error) {
+    console.log("data base not connected");
+  }
+  return redisClient;
 }
+
+function getdb() {
+  if ((!db)) {
+    throw new Error("mongodb not found not connected")
+  } else {
+    return db;
+  }
+}
+
+// close connections
+async function closeMongo() {
+  await mongoClient.close();
+  console.log("MongoDB connection closed");
+}
+async function closeRedis() {
+  await redisClient.quit();
+  console.log("Redis connection closed");
+}
+
+
 
 // Export des fonctions et clients
 module.exports = {
   // TODO: Exporter les clients et fonctions utiles
+  connectMongo,
+  connectRedis,
+  getdb,
+  closeMongo,
+  closeRedis,
 };
