2c4b7211138013af01febd870d0f2d9dae07185e
HICHAM DIOUANE <hichamdiouane9@gmail.com>
2025-01-13 19:22:16 +0100

    Tester les apis

--- src/app.js
+++ src/app.js
@@ -8,16 +8,24 @@ const config = require('./config/env');
 const db = require('./config/db');
 
 const courseRoutes = require('./routes/courseRoutes');
-const studentRoutes = require('./routes/studentRoutes');
 
 const app = express();
 
 async function startServer() {
   try {
-    // TODO: Initialiser les connexions aux bases de données
+    // TODO: Initialiser les connexions aux base de donnees
+    await db.connectMongo();
+    await db.connectRedis();
     // TODO: Configurer les middlewares Express
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
     // TODO: Monter les routes
+    app.use('/api', courseRoutes);
+
     // TODO: Démarrer le serveur
+    app.listen(config.port, () => {
+      console.log(`Server started on port ${config.port}`);
+    });
   } catch (error) {
     console.error('Failed to start server:', error);
     process.exit(1);
@@ -27,6 +35,13 @@ async function startServer() {
 // Gestion propre de l'arrêt
 process.on('SIGTERM', async () => {
   // TODO: Implémenter la fermeture propre des connexions
+  try {
+    await db.closeMongo();
+    await db.closeRedis();
+    console.log('Server shutting down...');
+  } catch (error) {
+    console.error('Failed to shutdown server:', error);
+  }
 });
 
 startServer();
